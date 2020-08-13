3085872cd48bec67d1c801434031708bcbb16843
Colin Ferm <colin@fortytwosolutions.com>
2020-08-13 13:08:54 +0200

    Added Disqus

--- StatsForSharksTemplate.php
+++ StatsForSharksTemplate.php
@@ -170,6 +170,11 @@ class StatsForSharksTemplate extends BaseTemplate {
 					}
 				?>
 				<?php $this->html( 'bodytext' ); ?>
+
+				<?php if ($skin->is_deal) : ?>
+					<br/><br/>
+					<div id="disqus_thread"></div>              
+				<?php endif; ?>
 				
 				<?php $this->html( 'catlinks' ); ?>
 				
@@ -319,6 +324,25 @@ class StatsForSharksTemplate extends BaseTemplate {
 		return t;
 		}(document, "script", "twitter-wjs"));
 	</script>
+	<script>
+	/**
+	*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+	*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+	(window.RLQ=window.RLQ||[]).push(function() {
+		var disqus_config = function () {
+			this.page.url = "<?php echo $skin->canonicalURL; ?>";  // Replace PAGE_URL with your page's canonical URL variable
+			this.page.identifier = mw.config.get('wgArticleId'); // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+		};
+		(function() { // DON'T EDIT BELOW THIS LINE
+		var d = document, s = d.createElement('script');
+		s.src = 'https://statsforsharks.disqus.com/embed.js';
+		s.setAttribute('data-timestamp', +new Date());
+		(d.head || d.body).appendChild(s);
+		})();
+	});
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+				
 </html>
 <?php
 	}
